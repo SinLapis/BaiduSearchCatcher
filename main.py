703faa36c1eba00103c30ea29674aca5caa22162
SinLapis <truthdragon@moegirl.org>
2015-09-25 13:41:15 +0900

    update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests, urllib, sys
 __author__ = 'SinLapis'
 
 key = '成电'
-url = 'http://www.baidu.com/s?wd=' + urllib.parse.quote(key)
-res = requests.get(url)
-print(res.text)
-# filename = 'html.txt'
-# html_file = open(filename, 'w')
-# html_file.write(res.text)
-# html_file.close()
+url = 'http://baike.baidu.com/search?word=' + urllib.parse.quote(key)
+headers = {'User-Agent': 'alexkh'}
+res = requests.get(url, headers=headers)
+filename = 'res.html'
+html_file = open(filename, 'w', encoding='utf-8')
+html_file.write(res.text.encode('latin1').decode('utf-8'))
+html_file.close()
